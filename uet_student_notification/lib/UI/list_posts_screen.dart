ef6560110acdcb9e22b1e3964b9e32af389f84ea
Son Tran <>
2020-10-14 15:00:01 +0700

    fix bug on ios

--- uet_student_notification/lib/UI/list_posts_screen.dart
+++ uet_student_notification/lib/UI/list_posts_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -41,7 +43,7 @@ class ListPostsScreen extends StatelessWidget {
           print("UET onMessage: $message");
           bloc.loadListPosts(context, false);
         },
-        onBackgroundMessage: myBackgroundMessageHandler,
+        onBackgroundMessage: Platform.isAndroid ? myBackgroundMessageHandler : null,
         onLaunch: (Map<String, dynamic> message) async {
           print("UET onLaunch: $message");
           bloc.loadListPosts(context, false);
